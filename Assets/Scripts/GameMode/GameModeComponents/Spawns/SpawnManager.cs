653f6089bb07a47ebb221f02e85c6b7f983509af
Dolpix <31806127+kikigoldwolf@users.noreply.github.com>
2023-02-20 13:09:50 +0000

    Feat: Teams System

--- Assets/Scripts/GameMode/GameModeComponents/Spawns/SpawnManager.cs
+++ Assets/Scripts/GameMode/GameModeComponents/Spawns/SpawnManager.cs
@@ -13,11 +13,30 @@ public class SpawnManager : MonoBehaviour{
             return _instance;
         } 
     }
+
+    [SerializeField] private Transform teamOneSpawn;
+    [SerializeField] private Transform teamTwoSpawn;
+
     private void Awake() {
         if (_instance != null && _instance != this) {
             Destroy(this);
         } else {
             _instance = this;
         }
+    }
+
+    public Vector3 GetTeamOneSpawn() {
+        if (teamOneSpawn == null) {
+            CustomLogger.LogError("No TeamOne spawn set!");
+            return Vector3.zero;
+        }
+        return teamOneSpawn.position;
+    }
+    public Vector3 GetTeamTwoSpawn() {
+        if (teamOneSpawn == null) {
+            CustomLogger.LogError("No TeamTwo spawn set!");
+            return Vector3.zero;
+        }
+        return teamTwoSpawn.position;
     }
 }
